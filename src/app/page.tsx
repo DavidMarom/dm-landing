257b8df31a4eb331b62c5015091fa94a19ff86a5
David Marom <davidmarom.pro@gmail.com>
2024-05-27 20:29:24 +0300

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,39 @@
 import Image from "next/image";
 
+const imageStyle = {
+  borderRadius: '50%',
+  width: '200px',
+  height: '200px',
+}
+
 export default function Home() {
   return (
     <div className="page-content">
-      <div className="ra">
+      <div className="rab">
 
-        <h1><p>David Marom</p></h1>
-        <h1><p>David Marom</p></h1>
+        <div className="">
+
+          <h1>David Marom</h1>
+          <h2>Web Developer</h2>
+          <br />
+          <br />
+          <p>Main stack: React, Node.js, Python</p>
+          <p>Also: Angular, Vue, PHP</p>
+          <p>Sql, Mongo | Mac, Ubuntu, Windows | AWS, Google</p>
+        </div>
+
+        <img src="/images/DM3.jpg" alt="David Marom" style={imageStyle} />
 
       </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div>
+        <h2>Services</h2>
+        <p></p>
+      </div>
+
     </div>
   );
 }
